bbdff04f9256ed57ce0adfb7a03386236526de0e
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2024-06-23 20:00:43 +0530

    main sections added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
+import Tech from "@/components/Tech";
+import Works from "@/components/Works";
 
 export default function Home() {
   return (
     <main>
       <NavBar/>
       <Hero/>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Contact/>
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
     <section className='h-[100vh]'>
+          <Spotlight className='-top-35 -left-39 md:-top-40 md:-left-50'/>
+          <Spotlight className='-top-35 -right-39 md:-top-40 md:-right-50'/>
         Hero
     </section>
   )
