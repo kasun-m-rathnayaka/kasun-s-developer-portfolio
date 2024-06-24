e1b6399352606c41461eee5f84bdbc9b9c5c2993
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2024-06-24 17:02:04 +0530

    chore: Add MagicButton component for hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,27 +2,31 @@ import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { BackgroundBeams } from "./ui/BackgroundBeams";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
 
 const Hero = () => {
   return (
     <section className="w-full h-screen">
-      <Spotlight
+      {/* <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
-      />
+      /> */}
       <BackgroundBeams />
-      <div className="w-[80%] mx-auto mt-24">
+      <div className="w-[90%] md:w-[50%] mx-auto mt-24">
         <div className="flex flex-col justify-center">
-          <h2 className="text-center text-sm">
+          <h2 className="text-center text-sm tracking-widest">
             Unleash web magic with next.js
           </h2>
           <TextGenerateEffect
-            className="text-center text-6xl"
+            className="text-center"
             words="Turning Creative Ideas into Interactive Realities."
           />
-          <p className="text-center">
+          <p className="text-center tracking-wider text-sm md:text-lg lg:text-2xl my-7">
             Hi, I am Kasun. a front end developer based in sri lanka
           </p>
+          <div className="w-[150px] block m-auto">
+            <MagicButton word="See My Work" />
+          </div>
         </div>
       </div>
     </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,17 @@ const config: Config = {
     },
     animation: {
       spotlight: "spotlight 2s ease .75s 1 forwards",
+      shimmer: "shimmer 2s linear infinite",
     },
     keyframes: {
+      shimmer: {
+        from: {
+          "backgroundPosition": "0 0"
+        },
+        to: {
+          "backgroundPosition": "-200% 0"
+        }
+      },
       spotlight: {
         "0%": {
           opacity: "0",
